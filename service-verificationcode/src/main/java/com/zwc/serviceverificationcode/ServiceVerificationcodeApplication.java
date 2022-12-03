d8d4423d9d0d6aba46f8d44cc3055ee5444d9ce3
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-12-03 18:26:15 +0900

    创建了service-verificationcode&注册到了nacos

--- service-verificationcode/src/main/java/com/zwc/serviceverificationcode/ServiceVerificationcodeApplication.java
+++ service-verificationcode/src/main/java/com/zwc/serviceverificationcode/ServiceVerificationcodeApplication.java
@@ -1,4 +1,4 @@
-package com.zwc.apipassenger;
+package com.zwc.serviceverificationcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiPassengerApplication {
+public class ServiceVerificationcodeApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ApiPassengerApplication.class);
+        SpringApplication.run(ServiceVerificationcodeApplication.class, args);
     }
+
 }
---
+++ service-verificationcode/src/test/java/com/zwc/serviceverificationcode/ServiceVerificationcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwc.serviceverificationcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceVerificationcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
