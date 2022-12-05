624f910979e4e95fb0938d595c8997642acf7a1e
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-12-05 20:51:49 +0900

    创建项目service-passenger-user

---
+++ service-passenger-user/src/main/java/com/zwc/servicepassengeruser/ServicePassengerApplication.java
@@ -0,0 +1,11 @@
+package com.zwc.servicepassengeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePassengerApplication.class);
+    }
+}
