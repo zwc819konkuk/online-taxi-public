99e7c0044f804d7852b9db3531f6e4dbe077f612
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-12-08 16:33:52 +0900

    service-passenger-user注册到nacos

--- service-passenger-user/src/main/java/com/zwc/servicepassengeruser/ServicePassengerApplication.java
+++ service-passenger-user/src/main/java/com/zwc/servicepassengeruser/ServicePassengerApplication.java
@@ -3,9 +3,11 @@ package com.zwc.servicepassengeruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.zwc.servicepassengeruser.mapper")
+@EnableDiscoveryClient
 public class ServicePassengerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServicePassengerApplication.class);
