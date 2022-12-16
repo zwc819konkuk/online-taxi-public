5fd69ddb7552b6b8afda160b70122805f160f8ce
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-12-16 19:54:53 +0900

    service-driver-user注册到nacos

--- service-driver-user/src/main/java/com/zwc/servicedriveruser/controller/UserController.java
+++ service-driver-user/src/main/java/com/zwc/servicedriveruser/controller/UserController.java
@@ -1,2 +1,22 @@
-package com.zwc.servicedriveruser.controller;public class UserController {
+package com.zwc.servicedriveruser.controller;
+
+import com.zwc.internalcommon.dto.DriverUser;
+import com.zwc.internalcommon.dto.ResponseResult;
+import com.zwc.servicedriveruser.service.DriverUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private DriverUserService driverUserService;
+
+    @PostMapping("/user")
+    public ResponseResult addUser(@RequestBody DriverUser driverUser) {
+        return driverUserService.addDriverUser(driverUser);
+    }
 }
--- service-driver-user/src/main/java/com/zwc/servicedriveruser/service/DriverUserService.java
+++ service-driver-user/src/main/java/com/zwc/servicedriveruser/service/DriverUserService.java
@@ -6,6 +6,9 @@ import com.zwc.servicedriveruser.mapper.DriverUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
 public class DriverUserService {
 
@@ -15,5 +18,13 @@ public class DriverUserService {
     public ResponseResult testGetDriveruser(){
         DriverUser driverUser = driverUserMapper.selectById(1);
         return ResponseResult.success(driverUser);
+    }
+
+    public ResponseResult addDriverUser(DriverUser driverUser){
+        LocalDateTime now = LocalDateTime.now();
+        driverUser.setGmtCreate(now);
+        driverUser.setGmtModified(now);
+        driverUserMapper.insert(driverUser);
+        return ResponseResult.success("");
     }
 }
