0b41060a07ffe95d012d18bd388a7e407ff194e1
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-12-16 20:49:51 +0900

    司机信息修改

--- service-driver-user/src/main/java/com/zwc/servicedriveruser/controller/UserController.java
+++ service-driver-user/src/main/java/com/zwc/servicedriveruser/controller/UserController.java
@@ -6,10 +6,7 @@ import com.zwc.servicedriveruser.service.DriverUserService;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -22,5 +19,10 @@ public class UserController {
     public ResponseResult addUser(@RequestBody DriverUser driverUser) {
         log.info(JSONObject.fromObject(driverUser).toString());
         return driverUserService.addDriverUser(driverUser);
+    }
+
+    @PutMapping("/user")
+    public ResponseResult updateUser(@RequestBody DriverUser driverUser){
+        return driverUserService.updateDriverUser(driverUser);
     }
 }
