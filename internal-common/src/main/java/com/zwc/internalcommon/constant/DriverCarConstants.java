a2e52fa72dbbcdafe99420c19964a8e8597d4499
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-12-25 21:23:07 +0900

    在service-driver-user插入司机的时候就初始化driver-user-work-status表

--- internal-common/src/main/java/com/zwc/internalcommon/constant/DriverCarConstants.java
+++ internal-common/src/main/java/com/zwc/internalcommon/constant/DriverCarConstants.java
@@ -27,4 +27,18 @@ public class DriverCarConstants {
      * 司机不存在
      */
     public static int DRIVER_NOT_EXIST = 0;
+
+    /**
+     * 司机工作状态：收车
+     */
+    public static int DRIVER_WORK_STATUS_STOP = 0;
+    /**
+     * 司机工作状态：出车
+     */
+    public static int DRIVER_WORK_STATUS_START = 1;
+    /**
+     * 司机工作状态：暂停
+     */
+    public static int DRIVER_WORK_STATUS_SUSPEND = 2;
+
 }
--- service-driver-user/src/main/java/com/zwc/servicedriveruser/service/DriverUserService.java
+++ service-driver-user/src/main/java/com/zwc/servicedriveruser/service/DriverUserService.java
@@ -3,8 +3,10 @@ package com.zwc.servicedriveruser.service;
 import com.zwc.internalcommon.constant.CommonStatusEnum;
 import com.zwc.internalcommon.constant.DriverCarConstants;
 import com.zwc.internalcommon.dto.DriverUser;
+import com.zwc.internalcommon.dto.DriverUserWorkStatus;
 import com.zwc.internalcommon.dto.ResponseResult;
 import com.zwc.servicedriveruser.mapper.DriverUserMapper;
+import com.zwc.servicedriveruser.mapper.DriverUserWorkStatusMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,9 @@ public class DriverUserService {
     @Autowired
     private DriverUserMapper driverUserMapper;
 
+    @Autowired
+    private DriverUserWorkStatusMapper driverUserWorkStatusMapper;
+
     public ResponseResult testGetDriveruser(){
         DriverUser driverUser = driverUserMapper.selectById(1);
         return ResponseResult.success(driverUser);
@@ -31,6 +36,16 @@ public class DriverUserService {
         driverUser.setGmtCreate(now);
         driverUser.setGmtModified(now);
         driverUserMapper.insert(driverUser);
+
+        //初始化 司机工作状态表
+        DriverUserWorkStatus driverUserWorkStatus = new DriverUserWorkStatus();
+        driverUserWorkStatus.setDriverId(driverUser.getId());
+        driverUserWorkStatus.setWorkStatus(DriverCarConstants.DRIVER_WORK_STATUS_STOP);
+        driverUserWorkStatus.setGmtModified(now);
+        driverUserWorkStatus.setGmtCreate(now);
+
+        driverUserWorkStatusMapper.insert(driverUserWorkStatus);
+
         return ResponseResult.success("");
     }
 
