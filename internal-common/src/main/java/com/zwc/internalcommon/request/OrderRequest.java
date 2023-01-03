fa865ee579d8ac40dbf3a0f13c8179d4023b9d62
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2023-01-03 19:20:45 +0900

    补充下单时的参数

--- internal-common/src/main/java/com/zwc/internalcommon/request/OrderRequest.java
+++ internal-common/src/main/java/com/zwc/internalcommon/request/OrderRequest.java
@@ -7,6 +7,11 @@ import java.time.LocalDateTime;
 
 @Data
 public class OrderRequest {
+    //乘客ID
+    private Long passengerId;
+
+    //乘客手机号
+    private String passengerPhone;
 
     //下单行政区域
     private String address;
--- service-order/src/main/java/com/zwc/serviceorder/service/OrderInfoService.java
+++ service-order/src/main/java/com/zwc/serviceorder/service/OrderInfoService.java
@@ -1,5 +1,6 @@
 package com.zwc.serviceorder.service;
 
+import com.zwc.internalcommon.constant.OrderConstants;
 import com.zwc.internalcommon.dto.OrderInfo;
 import com.zwc.internalcommon.dto.ResponseResult;
 import com.zwc.internalcommon.request.OrderRequest;
@@ -7,6 +8,8 @@ import com.zwc.serviceorder.mapper.OrderInfoMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -33,6 +36,12 @@ public class OrderInfoService  {
         OrderInfo orderInfo = new OrderInfo();
 
         BeanUtils.copyProperties(orderRequest,orderInfo);
+
+        orderInfo.setOrderStatus(OrderConstants.ORDER_START);
+
+        LocalDateTime now = LocalDateTime.now();
+        orderInfo.setGmtCreate(now);
+        orderInfo.setGmtModified(now);
 
         orderInfoMapper.insert(orderInfo);
 
